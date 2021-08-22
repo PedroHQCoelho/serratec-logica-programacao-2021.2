08e1f5fb37391699c881435497fc866188cfb7c3
PedroHQCoelho <pedrohqcoelho@gmail.com>
2021-08-22 14:26:38 -0300

    Add FaseDois

---
+++ FaseDois/indeciso.js
@@ -0,0 +1,12 @@
+/*
+O programa “indeciso” não sabe bem o número que quer imprimir. Por isso, antes de cada dúvida, os números devem ser separados do texto e atribuídos a uma variável inteira. 
+*/
+
+var n1 = 13;
+var n2 = 7;
+var n3 = 22;
+var n4 = 20;
+
+console.log("Quero te contar meu número da sorte! é " + n1 + ".");
+console.log("Não, espera, é " + n2 + ".. Ah esse era o da mamãe. O meu é " + n3 + ".");
+console.log("Já decidi: é "+ n4 + "! Bingo :)");
