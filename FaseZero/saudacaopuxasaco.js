b37576153b1965b6f4468ebb76fc5b46f75be44d
PedroHQCoelho <pedrohqcoelho@gmail.com>
2021-08-22 12:47:03 -0300

    Add FaseZero

---
+++ FaseZero/saudacaopuxasaco.js
@@ -0,0 +1,9 @@
+/*
+O programa 'Saudação puxa-saco' faz todos os cumprimentos do dia de uma vez só, para a semana toda! Serão mostradas as três mensagens de tela, uma após a outra, por dia, de segunda a sexta, cada dia em uma linha.
+*/
+
+console.log("Segunda-feira\nBom Dia!\nBoa Tarde!\nBoa Noite!");
+console.log("Terça-feira\nBom Dia!\nBoa Tarde!\nBoa Noite!");
+console.log("Quarta-feira\nBom Dia!\nBoa Tarde!\nBoa Noite!");
+console.log("Quinta-feira\nBom Dia!\nBoa Tarde!\nBoa Noite!");
+console.log("Sexta-feira\nBom Dia!\nBoa Tarde!\nBoa Noite!");
